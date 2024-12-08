174d4daf61ee2ee237b59f271adc6f2c3c17c24f
Juan Vasquez <juancvasdisenho@gmail.com>
2024-12-08 15:52:15 -0400

    swrhghg

--- benefits-content.js
+++ benefits-content.js
@@ -18,7 +18,7 @@ const benefitshtmlContent = `
             <h2>Once a month you get</h2>
             <ul>
                 <li class="star"><b>Digital tactics report:</b> So you can learn about the most effective current marketing strategies, along with a step-by-step guide to help you use them too.</li>
-                <li class="star">One-on-One Consulting: <b>A live analysis of a subscriber's content strategy (selected at random, so just by being part of it, you have a chance to receive feedback on your content strategy).</b></li>
+                <li class="star"><b>One-on-One Consulting</b>: A live analysis of a subscriber's content strategy (selected at random, so just by being part of it, you have a chance to receive feedback on your content strategy).</li>
             </ul>
         </div>
     </section>
