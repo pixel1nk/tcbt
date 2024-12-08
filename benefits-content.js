10e7c71505658aa00620e2cd010f124b238e509f
Juan Vasquez <juancvasdisenho@gmail.com>
2024-12-07 20:29:02 -0400

    yu77

---
+++ benefits-content.js
@@ -0,0 +1,19 @@
+// Selecciona el elemento donde deseas insertar el contenido
+const mainContent = document.getElementById('benefits-content');
+
+// Define el HTML que deseas insertar
+const htmlContent = `
+<section class="benefits">
+        <div class="container">
+            <h2>For each question you get</h2>
+            <ul>
+                <li class="star"><b>Expert-Backed Answer:</b> Well-structured, informative content based on reliable sources, providing a solid foundation for your expertise.</li>
+                <li class="star">Content adapted for <b>Facebook, YouTube, Blog, TikTok and Instagram</b></li>
+            </ul>
+        </div>
+    </section>
+
+`;
+
+// Inserta el contenido dentro del elemento
+mainContent.innerHTML = htmlContent;
---
+++ footer-content.js
@@ -0,0 +1,15 @@
+// Selecciona el elemento donde deseas insertar el contenido
+const mainContent = document.getElementById('footer-content');
+
+// Define el HTML que deseas insertar
+const htmlContent = `
+<footer class="footer">
+        <div class="container">
+            <p>© 2024 The Content Bang Theory. All rights reserved.</p>
+        </div>
+    </footer>
+
+`;
+
+// Inserta el contenido dentro del elemento
+mainContent.innerHTML = htmlContent;
